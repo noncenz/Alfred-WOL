4e629734c39de794e03650303d18485745f0498f
noncenz <noncenz@ultibits.com>
2023-08-16 21:23:32 -0400

    Initial POC

---
+++ filter_list.py
@@ -0,0 +1,79 @@
+import pickle
+
+from json import JSONEncoder
+import sys
+import json
+
+
+name = ""
+macaddr = ""
+ipaddr = ""
+query = ""
+
+x = len(sys.argv) # not using match fpr python3 < 3.10 compatability
+if x == 2:
+    macaddr = sys.argv[1]
+    query = macaddr.lower()
+elif x == 3:
+    name = sys.argv[1]
+    macaddr = sys.argv[2]
+    query = name.lower()
+elif x == 4:
+    name = sys.argv[1]
+    macaddr = sys.argv[2]
+    ipaddr = sys.argv[3]
+    query = name
+
+class Server:
+    def __init__(self, name="", ipaddr="", macaddr=""):
+        self.name = name
+        self.ipaddr = ipaddr
+        self.macaddr = macaddr
+
+
+class ServerList:
+    def __init__(self, servers=[]):
+        self.servers = servers
+
+# todo: cache?
+with open("servers", "a+b") as f:
+    try:
+        f.seek(0)
+        server_list = pickle.load(f)
+    except:
+        server_list = ServerList()
+    f.close()
+
+
+query = query.lower()
+result_list = []
+
+exactMatch = False
+thisQueryTitle = title = (name + " " + macaddr + " " + ipaddr).strip()
+for s in server_list.servers:
+
+    if s.macaddr.lower().startswith(query) or s.name.lower().startswith(query):
+        title = (s.name + " " + s.macaddr + " " + s.ipaddr).strip()
+        if title == thisQueryTitle:
+            exactMatch = True
+        variables = {"name": s.name, "macaddr": s.macaddr, "ipaddr": s.ipaddr}
+        isValid = True
+        # mods = {"cmd": {"valid": isValid}}
+        match = {"title": title, "subtitle": s.name, "autocomplete": title, "arg": s.macaddr, "variables": variables}
+
+        result_list.append(match)
+
+if not exactMatch:
+    variables = {"name": name, "macaddr": macaddr, "ipaddr": ipaddr}
+    thisQuery = {"title": thisQueryTitle, "subtitle": name, "autocomplete": thisQueryTitle, "arg": macaddr, "variables": variables}
+    result_list.append(thisQuery)
+
+result = {"items": result_list}
+
+print(json.dumps(result))
+
+
+
+
+
+
---
+++ save_server.py
@@ -0,0 +1,43 @@
+import sys
+import pickle
+import os
+
+name = os.getenv("name")
+macaddr = os.getenv("macaddr")
+ipaddr = os.getenv("ipaddr")
+
+class Server:
+    def __init__(self, name="", ipaddr="", macaddr=""):
+        self.name = name
+        self.ipaddr = ipaddr
+        self.macaddr = macaddr
+
+class ServerList:    
+    def __init__(self, servers=[]):
+        self.servers = servers
+
+def object_exists(server_list, query):
+    for server in server_list:
+        if server.name == query.name and server.ipaddr == query.ipaddr and server.macaddr == query.macaddr:
+            return True
+    return False
+
+
+
+with open("servers", "a+b") as f:
+    try:
+        f.seek(0)
+        server_list = pickle.load(f)
+    except:
+        server_list = ServerList()
+    finally:    
+        s = Server(name=name, macaddr=macaddr, ipaddr=ipaddr)
+
+        if not (object_exists(server_list.servers,s)):
+            server_list.servers.append(s)
+            f.seek(0)
+            f.truncate()
+            pickle.dump(server_list, f)
+    f.close()
+
+print(macaddr)
